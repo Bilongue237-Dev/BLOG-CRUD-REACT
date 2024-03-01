661d2c32ee7212cb6c7247ccab2c1bd27aea14f4
bilongue237 <tchafla100@gmail.com>
2024-03-01 09:39:36 -0600

    text area carracter length

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
 import Navigation from "../components/Navigation";
 
 function Blog() {
+  const [content, setContent] = useState("");
+  const [error, setError] = useState(true);
+
+  const handleSumit = (e) => {
+    e.preventDefault();
+
+    if (content.length < 140) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  };
   return (
     <div className="blog-container">
       <Logo />
       <Navigation />
       <h1>BLOG</h1>
 
-      <form>
+      <form onSubmit={(e) => handleSumit(e)}>
         <input type="text" placeholder="Nom"></input>
-        <textarea placeholder="Message"></textarea>
+        <textarea
+          placeholder="Message"
+          onChangeCapture={(e) => setContent(e.target.value)}
+        ></textarea>
+        {error && <p>Veuillez écrire un minimum de 140 carractères </p>}
         <input type="submit" className="submit" value="Envoyer"></input>
       </form>
       <ul></ul>
